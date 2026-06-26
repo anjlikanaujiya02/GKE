dcde37ebcc40fa928952203128732eb2508ddb59
anjlikanaujiya02 <anjlikanaujiya02@gmail.com>
2026-06-26 15:31:20 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,11 @@ output "cluster_name" {
 }
 
 output "cluster_endpoint" {
-  value = module.gke.endpoint
+  value     = module.gke.endpoint
+  sensitive = true
+}
+
+output "cluster_ca" {
+  value     = module.gke.ca_certificate
+  sensitive = true
 }
